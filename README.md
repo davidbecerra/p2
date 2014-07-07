1797ffdbac991909b3e4a371014ca23629dd5120
David Becerra <dbecerra@college.harvard.edu>
2014-07-07 00:39:52 -0400

    Added large dictionary. Logic now reads in dictionary for password

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ By David Becerra
 *rtrim : http://www.php.net/manual/en/function.rtrim.php
 *count : http://www.php.net/manual/en/function.count.php
 *forms, required forms, form validation, : w3schools
+*dictionary of words: Courtesy of Maxim Sukharev-Chuyan (a fellow student in the class) URL: https://github.com/maximsc/p2/blob/master/wordlists/words9.txt
--- pass-gen.php
+++ pass-gen.php
@@ -1,26 +1,28 @@
 <?php
 
-$wordbank = [
-	'the',
-	'cat', 
-	'computer', 
-	'he', 
-	'because', 
-	'independently',
-	'zoo',
-	'alphabet',
-	'science',
-	'Turing',
-	'foo',
-	'garlic'
-];
+$dictionary = file('dictionary.txt', FILE_IGNORE_NEW_LINES);
+
+// $wordbank = [
+// 	'the',
+// 	'cat', 
+// 	'computer', 
+// 	'he', 
+// 	'because', 
+// 	'independently',
+// 	'zoo',
+// 	'alphabet',
+// 	'science',
+// 	'Turing',
+// 	'foo',
+// 	'garlic'
+// ];
 
 
 $nwords = (int) $_GET['nwords'];
 
 $password = '';
 for ($i = 0; $i < $nwords; $i++) {
-	$password .= $wordbank[rand(0, count($wordbank) - 1)] . '-';
+	$password .= $dictionary[rand(0, count($dictionary) - 1)] . '-';
 }
 
 $password = rtrim($password, '-');
